6a891b677334cc9956ac7d4d3e9572a5346ebf1f
Nattapon Jutamas <nattaponj@outlook.com>
2025-05-20 22:24:04 +0700

    Add Navigation Links

--- 7-next-1/my-app/app/layout.tsx
+++ 7-next-1/my-app/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { NavLinks } from "./ui/nav-links";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavLinks />
         <div>Main Layout</div>
         <div>{children}</div>
       </body>
