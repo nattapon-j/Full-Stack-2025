c27f380de6d299b334a20ba9401e65707e27e6ec
Nattapon Jutamas <nattaponj@outlook.com>
2025-05-24 22:22:47 +0700

    Add 'Itim' - (Google) font

--- 7-next-1/my-app/app/layout.tsx
+++ 7-next-1/my-app/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -33,6 +38,6 @@ export default function RootLayout({
         <div>Main Layout</div>
         <div>{children}</div>
       </body>
-    </html>
+    </html >
   );
 }
