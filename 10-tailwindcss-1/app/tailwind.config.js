2e937ff2f768af0ee5e6a1252cdc0d92b5340fcf
Nattapon Jutamas <nattaponj@outlook.com>
2025-05-25 09:41:21 +0700

    10-Tailwind CSS-1 - Init

---
+++ 10-tailwindcss-1/app/tailwind.config.js
@@ -0,0 +1,15 @@
+// npm init -y
+// npm install tailwindcss@3 postcss autoprefixer -D 
+// npx tailwindcss init 
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html.js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
